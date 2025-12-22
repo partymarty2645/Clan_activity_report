28cfcb0f7bc75b7b7447615983f49133d38b4e88
partymarty2645 <marty.vandenberk@hotmail.com>
2025-12-22 16:28:55 +0100

    Cleanup: Remove temporary check_indexes.py script

--- check_indexes.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('clan_data.db')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='index'")
-indexes = cursor.fetchall()
-print("Existing indexes:")
-for idx in indexes:
-    print(f"  - {idx[0]}")
-conn.close()
