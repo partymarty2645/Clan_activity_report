7dd7a85f3cb844e99b6eefac696aa3bacd0f264b
partymarty2645 <marty.vandenberk@hotmail.com>
2025-12-22 16:01:55 +0100

    Remove temporary check scripts

--- check_version.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('clan_data.db')
-cursor = conn.cursor()
-cursor.execute("SELECT version_num FROM alembic_version")
-version = cursor.fetchone()
-print(f"Current Alembic version: {version[0] if version else 'None'}")
-conn.close()
